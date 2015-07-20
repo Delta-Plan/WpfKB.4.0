ae6cdb7ffdb0160b7eb285c5859b4e2a4c362663
delta.plan <delta.plan@datakrat.ru>
2015-07-20 17:28:33 +0500

    fix залипания шифта и глюков капслока

--- WpfKb/LogicalKeys/TogglingModifierKey.cs
+++ WpfKb/LogicalKeys/TogglingModifierKey.cs
@@ -15,7 +15,8 @@ namespace WpfKb.LogicalKeys
             // This is a bit tricky because we can only get the state of a toggling key after the input has been
             // read off the MessagePump.  Ergo if we make that assumption that in the time it takes to run this method
             // we will be toggling the state of the key, set IsInEffect to the new state and then press the key.
-            IsInEffect = !InputSimulator.IsTogglingKeyInEffect(KeyCode);
+            //IsInEffect = !InputSimulator.IsTogglingKeyInEffect(KeyCode);
+	        IsInEffect = !IsInEffect;
             base.Press();
         }
 
